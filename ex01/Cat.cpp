df01f64c8d66168e555e1cadbec9e99dde86c25c
lbarreto <luizfernandobarretodasilva@gmail.com>
2025-10-17 14:09:04 -0300

    Feat: Started ex01

---
+++ ex01/Cat.cpp
@@ -0,0 +1,27 @@
+#include "Cat.hpp"
+
+Cat::Cat () {
+    std::cout << "A Cat has been created!" << std::endl;
+    this->type = "Cat";
+}
+
+Cat::Cat (const Cat &copy) {
+    std::cout << "Cat copy constructor called" << std::endl;
+    *this = copy;
+}
+
+Cat& Cat::operator=(const Cat &copy) {
+    std::cout << "Cat copy assignment operator called" << std::endl;
+    if (this != &copy)
+        this->type = copy.type;
+    return (*this);
+}
+
+Cat::~Cat () {
+    std::cout << "A Cat just got destroyed!" << std::endl;
+}
+
+void    Cat::makeSound() const {
+    std::cout << "Meow meow! 🐱" << std::endl;
+}
+
